6ca93e71b8c14e53573a806f891d8cd0a8d127ee
Kostyshyn <kostyshyn@ua.fm>
2018-03-14 18:24:38 +0200

    [+] add axios and render posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import './style.css';
 
+import axios from 'axios'
+
+const uri = "http://jsonplaceholder.typicode.com/posts";
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello world!</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+        	posts: []
+        }
+    }
+    componentDidMount(){
+		axios.get(uri).then((response) => {
+			this.setState({
+            	posts: response.data
+            });
+            console.log(this.state.posts)
+		}).catch((err) => {
+			console.log(err);
+		})   	
+    }
+	render(){
+		return (
+			<div className="posts-wrapper">
+				{
+				this.state.posts.map((post) => {
+					return ( 
+						<div key={ post.id } className="post">
+							<h3>{ post.title }</h3>
+							<p className="post-body">{ post.body }</p>
+						</div>
+						)
+				})
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
